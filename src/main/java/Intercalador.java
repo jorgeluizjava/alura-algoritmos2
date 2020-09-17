ff85595a5393fc5a522e0145c1f18648583001b6
JORGE GALVAO <jorgeluizjava@gmail.com>
2020-09-16 23:49:14 -0300

    Adicionado cenário de teste

---
+++ src/main/java/Intercalador.java
@@ -0,0 +1,5 @@
+public class Intercalador {
+    public Nota[] intercala(Nota[] notas1, Nota[] notas2) {
+        return new Nota[0];
+    }
+}
--- src/test/java/IntercalaDoisArraysJaOrdenados.java
+++ src/test/java/IntercalaDoisArraysJaOrdenados.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class IntercalaDoisArraysJaOrdenados {
 
     @Test
@@ -19,9 +23,21 @@ public class IntercalaDoisArraysJaOrdenados {
                 new Nota("ana", 10)
         };
 
+        Intercalador intercalador = new Intercalador();
+        Nota[] notasIntercaladas = intercalador.intercala(notas1, notas2);
 
+        assertThat(notasIntercaladas.length, is(equalTo(9)));
+
+        assertThat(notasIntercaladas[0].getValor(), is(equalTo(3d)));
+        assertThat(notasIntercaladas[1].getValor(), is(equalTo(4d)));
+        assertThat(notasIntercaladas[2].getValor(), is(equalTo(5d)));
+        assertThat(notasIntercaladas[3].getValor(), is(equalTo(6.7d)));
+        assertThat(notasIntercaladas[4].getValor(), is(equalTo(7d)));
+        assertThat(notasIntercaladas[5].getValor(), is(equalTo(8.5d)));
+        assertThat(notasIntercaladas[6].getValor(), is(equalTo(9d)));
+        assertThat(notasIntercaladas[7].getValor(), is(equalTo(9.3d)));
+        assertThat(notasIntercaladas[8].getValor(), is(equalTo(10d)));
 
     }
-
 
 }
